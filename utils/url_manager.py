53c589dd5c194e9bfc018a33f2bf6694a8625d37
michael.wan <michael.wan@ichub.com>
2023-02-23 16:01:57 +0800

    2023-02-23 爬虫练习 commit

---
+++ utils/url_manager.py
@@ -0,0 +1,15 @@
+class UrlManager:
+    """
+    Url管理器
+    """
+    def __init__(self):
+        self.new_urls = set()
+        self.old_urls = set()
+    def add_new_url(self,url):
+        pass
+    def add_new_urls(self,urls):
+        pass
+    def get_url(self):
+        pass
+    def has_new_url(self):
+        pass
