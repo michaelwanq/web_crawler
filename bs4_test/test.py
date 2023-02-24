123b721adacea2cc512a6ff2757e6154b0449aee
michael.wan <michael.wan@ichub.com>
2023-02-24 10:46:54 +0800

    2023-02-24 10:46 beautifulsoup练习 commit

--- bs4_test/test.py
+++ bs4_test/test.py
@@ -1,8 +1,19 @@
 from bs4 import BeautifulSoup
-with open('./test.html') as fin:
+
+with open('./test.html', encoding='utf-8') as fin:
     html_doc = fin.read()
 
 print(html_doc)
 print('#' * 30)
 
-soup = BeautifulSoup(html_doc,"html.parser")
+soup = BeautifulSoup(html_doc, "html.parser")
+div_node = soup.find('div',id='content')
+print(div_node)
+print("#" * 30)
+
+links = div_node.find_all('a')
+for link in links:
+    print(link.name, link['href'], link.getText())
+
+img = div_node.find("img")
+print(img["src"])
